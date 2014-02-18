c85a01d02cb2e9de5629833495cce5984b0459d4
cyberfunk <hello@cyberfunk.org>
2014-02-18 22:33:58 +0800

    Add initial logic for traversing files.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+var async = require('async');
 var EventEmitter = require('events').EventEmitter;
 var fs = require('fs');
-var path = require('path');
+var path = require('path'), basename = path.basename;
 var util = require('util');
+var utils = require('cli-fs');
 
+var EXTENSION = 'rc';
 var JSON_TYPE = 'json';
 var INI_TYPE = 'ini';
 var types = [JSON_TYPE, INI_TYPE];
@@ -18,21 +21,51 @@ var types = [JSON_TYPE, INI_TYPE];
 var RunControl = function(options) {
   EventEmitter.call(this);
   options = options || {};
+  this.rc = {};
   this.type = options.type || JSON_TYPE;
-  // TODO: validate name and path
-  this.name = options.name;
-  this.path = options.path;
+  this.name = options.name || basename(process.argv[1]) + EXTENSION;
+  this.path = options.path || this.getDefaultSearchPath();
 }
 
 util.inherits(RunControl, EventEmitter);
+
+/**
+ *  Retrieve an array for the default search path.
+ *
+ *  @api private
+ *
+ *  @return An array of default search paths.
+ */
+RunControl.prototype.getDefaultSearchPath = function() {
+  var pth = [];
+  // this library should be in node_modules/cli-rc so this should
+  // resolve to the package that depends upon this library
+  var pkg = path.normalize(path.join(__dirname, '..', '..'));
+  var usr = utils.home();
+  pth.push(pkg, usr);
+  return pth;
+}
 
 /**
  *  Load the configuration files.
  *
  *  @api public
  */
-RunControl.prototype.load = function() {
-
+RunControl.prototype.load = function(callback) {
+  var files = this.path.slice(0), name = this.name;
+  var rc = this.rc;
+  files.forEach(function(dir, index, arr) {
+    arr[index] = path.join(dir, name);
+  })
+  //console.dir(files);
+  async.mapSeries(files, function(file, callback) {
+    fs.exists(file, function(exists) {
+      if(!exists) return callback();
+    });
+  }, function(err, results) {
+    //console.log('rc load complete');
+    callback(rc);
+  });
 }
 
 /**
--- test/unit/json.js
+++ test/unit/json.js
@@ -2,9 +2,19 @@ var expect = require('chai').expect;
 var rc = require('../..');
 
 describe('cli-util:', function() {
-  it('should load runtime configuration (json)', function(done) {
+  it('should set default options', function(done) {
     var loader = rc();
-    loader.load();
+    expect(loader.type).to.eql(rc.JSON);
+    expect(loader.name).to.eql('_mocharc');
+    expect(loader.path).to.be.an('array');
+    expect(loader.path.length).to.eql(2);
     done();
+  });
+  it('should load empty runtime configuration (json)', function(done) {
+    var loader = rc();
+    loader.load(function loaded(rc) {
+      expect(rc).to.eql({});
+      done();
+    });
   });
 })
